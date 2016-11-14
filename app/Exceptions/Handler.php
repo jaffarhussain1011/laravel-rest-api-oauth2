faada59fe127d719660413afa465a8665fa6fe91
Jaffar Hussain <jaffarhussain1011@gmail.com>
2016-11-14 23:06:12 +0500

    handler error render as json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,45 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        // return parent::render($request, $exception);
+        $response = [
+        'response'=>'fail',
+        'errorMsg' => $exception->getMessage()?$exception->getMessage():'Bad Request',
+        'statusCode'=>is_callable(array($exception, 'getStatusCode'))?$exception->getStatusCode():400
+    ];
+
+      // if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+      //     $response['errorMsg']='Token Expired';
+      // } else if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+      //     $response['errorMsg']='Token Invalid';
+      // }else
+      if($exception instanceof \Illuminate\Http\Exception\HttpResponseException){
+          $exceptionResponse = is_callable(array($exception, 'getResponse'))?$exception->getResponse():null;
+          $exceptionData = is_callable(array($exceptionResponse, 'getData'))?$exceptionResponse->getData():null;
+          if($exceptionData){
+            $response['errorMsg']='Field Error(s)';
+            $errors = [];
+            foreach ($exceptionData as $field => $error) {
+              $errors[] = ['field'=>$field,'errors'=>$error];
+            }
+            $response['fieldErrors'] = $errors;
+            // $response['fieldErrors'] = $exceptionData;
+            $response['statusCode'] = 200;
+          }else{
+            $exceptionContent = is_callable(array($exceptionResponse, 'getContent'))?$exceptionResponse->getContent():'Error';
+            $response['errorMsg']=$exceptionContent;
+          }
+
+      }
+      if(config('app.debug')){
+        $response['debug'] = [
+          'file' => is_callable(array($exception, 'getFile'))?($exception->getFile()):'',
+          'line'=>is_callable(array($exception, 'getLine'))?($exception->getLine()):'',
+          'trace'=>is_callable(array($exception, 'getTraceAsString'))?($exception->getTraceAsString()):'',
+      ];
+      }
+      //this is api so send only json
+      return response()->json($response, $response['statusCode']);
     }
 
     /**
